3bf344083e44408ff7535fb4a6f1eea21fb2b1bb
Thomas Pronold <tp@zotorn.de>
2020-04-09 17:36:49 +0200

    First fully working docker images

---
+++ build-push-upload.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+set -e
+
+## CONFIG
+VERSION=0.0.1
+DOCKER_HUB_ACCOUNT=zotornit
+DOCKER_HUB_IMAGE=t3ter-upload
+
+
+docker build --file ./Dockerfile.upload -t $DOCKER_HUB_ACCOUNT/$DOCKER_HUB_IMAGE:$VERSION .
+docker image tag $DOCKER_HUB_ACCOUNT/$DOCKER_HUB_IMAGE:$VERSION $DOCKER_HUB_ACCOUNT/$DOCKER_HUB_IMAGE:latest
+
+# push latest and $VERSION
+docker image push $DOCKER_HUB_ACCOUNT/$DOCKER_HUB_IMAGE
+
+
+
+
